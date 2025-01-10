a34d7e84f9f22cfa0dce99f38f6f4768811f2d98
caoqiming.cpp <caoqiming.cpp@bytedance.com>
2025-01-10 19:57:21 +0800

    add gem puzzle 4*4

--- encoding_schemes/gem_puzzle.js
+++ encoding_schemes/gem_puzzle.js
@@ -1,7 +1,9 @@
 // 设置棋盘的大小
 const boardSize = 8;
+const boardSize_4 = 4;
 // 计算每个格子的大小
 let cellSize;
+let cellSize_4;
 
 // 定义一个包含6种颜色的列表
 const colors = [
@@ -38,7 +40,8 @@ function sketch1(p) {
     // 设置文本对齐方式为居中
     p.textAlign(p.CENTER, p.CENTER);
     // 设置文本大小
-    p.textSize(8);
+    p.textSize(10);
+    p.frameRate(5);
   };
   p.draw = function () {
     // 设置背景颜色为白色
@@ -83,6 +86,7 @@ function sketch2(p) {
     p.textAlign(p.CENTER, p.CENTER);
     // 设置文本大小
     p.textSize(10);
+    p.frameRate(5);
   };
   p.draw = function () {
     // 设置背景颜色为白色
@@ -136,6 +140,7 @@ function sketch3(p) {
     p.textAlign(p.CENTER, p.CENTER);
     // 设置文本大小
     p.textSize(10);
+    p.frameRate(5);
   };
   p.draw = function () {
     // 设置背景颜色为白色
@@ -184,6 +189,7 @@ function sketch4(p) {
     p.textAlign(p.CENTER, p.CENTER);
     // 设置文本大小
     p.textSize(10);
+    p.frameRate(5);
   };
   p.draw = function () {
     // 设置背景颜色为白色
@@ -230,6 +236,7 @@ function sketch5(p) {
     p.textAlign(p.CENTER, p.CENTER);
     // 设置文本大小
     p.textSize(10);
+    p.frameRate(5);
   };
   p.draw = function () {
     // 设置背景颜色为白色
@@ -275,6 +282,7 @@ function sketch6(p) {
     p.textAlign(p.CENTER, p.CENTER);
     // 设置文本大小
     p.textSize(10);
+    p.frameRate(5);
   };
   p.draw = function () {
     // 设置背景颜色为白色
@@ -320,6 +328,7 @@ function sketch7(p) {
     p.textAlign(p.CENTER, p.CENTER);
     // 设置文本大小
     p.textSize(10);
+    p.frameRate(5);
   };
   p.draw = function () {
     // 设置背景颜色为白色
@@ -365,6 +374,7 @@ function sketch8(p) {
     p.textAlign(p.CENTER, p.CENTER);
     // 设置文本大小
     p.textSize(10);
+    p.frameRate(5);
   };
   p.draw = function () {
     // 设置背景颜色为白色
@@ -404,3 +414,242 @@ new p5(sketch5);
 new p5(sketch6);
 new p5(sketch7);
 new p5(sketch8);
+
+// 以下为4*4的部分
+
+function sketch4_1(p) {
+  p.setup = function () {
+    // 找到想把该画布插入的位置
+    let divElement = document.getElementById("chessboard4");
+    let canvas = p.createCanvas(200, 200);
+    canvas.parent(divElement);
+
+    // 计算每个格子的大小
+    cellSize_4 = p.width / boardSize_4;
+    // 设置文本对齐方式为居中
+    p.textAlign(p.CENTER, p.CENTER);
+    // 设置文本大小
+    p.textSize(10);
+    p.frameRate(5);
+  };
+  p.draw = function () {
+    // 设置背景颜色为白色
+    p.background(255);
+    // 遍历棋盘的每一行
+    for (let row = 0; row < boardSize_4; row++) {
+      // 遍历棋盘的每一列
+      for (let col = 0; col < boardSize_4; col++) {
+        // 计算当前格子的索引
+        let index = row * boardSize_4 + col;
+        // 根据索引的奇偶性来设置格子的填充颜色
+        p.fill((index + row) % 2 === 0 ? 200 : 255);
+        // 绘制当前格子
+        p.rect(col * cellSize_4, row * cellSize_4, cellSize_4, cellSize_4);
+        // 设置文本颜色为黑色
+        p.fill(0);
+        // 将数字转换为长度为6的二进制字符串
+        let binaryIndex = index.toString(2).padStart(4, "0");
+        // 在当前格子中显示二进制数字
+        p.text(
+          binaryIndex,
+          col * cellSize_4 + cellSize_4 / 2,
+          row * cellSize_4 + cellSize_4 / 2
+        );
+      }
+    }
+  };
+}
+
+// index-0
+function sketch4_2(p) {
+  p.setup = function () {
+    // 找到想把该画布插入的位置
+    let divElement = document.getElementById("chessboard4-index-0");
+    let canvas = p.createCanvas(200, 200);
+    canvas.parent(divElement);
+
+    // 计算每个格子的大小
+    cellSize_4 = p.width / boardSize_4;
+    // 设置文本对齐方式为居中
+    p.textAlign(p.CENTER, p.CENTER);
+    // 设置文本大小
+    p.textSize(10);
+    p.frameRate(5);
+  };
+  p.draw = function () {
+    // 设置背景颜色为白色
+    p.background(255);
+    // 遍历棋盘的每一行
+    for (let row = 0; row < boardSize_4; row++) {
+      // 遍历棋盘的每一列
+      for (let col = 0; col < boardSize_4; col++) {
+        // 计算当前格子的索引
+        let index = row * boardSize_4 + col;
+        // 根据索引的奇偶性来设置格子的填充颜色
+        p.fill((index + row) % 2 === 0 ? 200 : 255);
+        if (isBitSet(index, 0)) {
+          p.fill(colors[0]);
+        }
+        // 绘制当前格子
+        p.rect(col * cellSize_4, row * cellSize_4, cellSize_4, cellSize_4);
+        // 设置文本颜色为黑色
+        p.fill(0);
+        // 将数字转换为长度为6的二进制字符串
+        let binaryIndex = index.toString(2).padStart(4, "0");
+        // 在当前格子中显示二进制数字
+        p.text(
+          binaryIndex,
+          col * cellSize_4 + cellSize_4 / 2,
+          row * cellSize_4 + cellSize_4 / 2
+        );
+      }
+    }
+  };
+}
+
+// index-1
+function sketch4_3(p) {
+  p.setup = function () {
+    // 找到想把该画布插入的位置
+    let divElement = document.getElementById("chessboard4-index-1");
+    let canvas = p.createCanvas(200, 200);
+    canvas.parent(divElement);
+
+    // 计算每个格子的大小
+    cellSize_4 = p.width / boardSize_4;
+    // 设置文本对齐方式为居中
+    p.textAlign(p.CENTER, p.CENTER);
+    // 设置文本大小
+    p.textSize(10);
+    p.frameRate(5);
+  };
+  p.draw = function () {
+    // 设置背景颜色为白色
+    p.background(255);
+    // 遍历棋盘的每一行
+    for (let row = 0; row < boardSize_4; row++) {
+      // 遍历棋盘的每一列
+      for (let col = 0; col < boardSize_4; col++) {
+        // 计算当前格子的索引
+        let index = row * boardSize_4 + col;
+        // 根据索引的奇偶性来设置格子的填充颜色
+        p.fill((index + row) % 2 === 0 ? 200 : 255);
+        if (isBitSet(index, 1)) {
+          p.fill(colors[1]);
+        }
+        // 绘制当前格子
+        p.rect(col * cellSize_4, row * cellSize_4, cellSize_4, cellSize_4);
+        // 设置文本颜色为黑色
+        p.fill(0);
+        // 将数字转换为长度为6的二进制字符串
+        let binaryIndex = index.toString(2).padStart(4, "0");
+        // 在当前格子中显示二进制数字
+        p.text(
+          binaryIndex,
+          col * cellSize_4 + cellSize_4 / 2,
+          row * cellSize_4 + cellSize_4 / 2
+        );
+      }
+    }
+  };
+}
+
+// index-2
+function sketch4_4(p) {
+  p.setup = function () {
+    // 找到想把该画布插入的位置
+    let divElement = document.getElementById("chessboard4-index-2");
+    let canvas = p.createCanvas(200, 200);
+    canvas.parent(divElement);
+
+    // 计算每个格子的大小
+    cellSize_4 = p.width / boardSize_4;
+    // 设置文本对齐方式为居中
+    p.textAlign(p.CENTER, p.CENTER);
+    // 设置文本大小
+    p.textSize(10);
+    p.frameRate(5);
+  };
+  p.draw = function () {
+    // 设置背景颜色为白色
+    p.background(255);
+    // 遍历棋盘的每一行
+    for (let row = 0; row < boardSize_4; row++) {
+      // 遍历棋盘的每一列
+      for (let col = 0; col < boardSize_4; col++) {
+        // 计算当前格子的索引
+        let index = row * boardSize_4 + col;
+        // 根据索引的奇偶性来设置格子的填充颜色
+        p.fill((index + row) % 2 === 0 ? 200 : 255);
+        if (isBitSet(index, 2)) {
+          p.fill(colors[2]);
+        }
+        // 绘制当前格子
+        p.rect(col * cellSize_4, row * cellSize_4, cellSize_4, cellSize_4);
+        // 设置文本颜色为黑色
+        p.fill(0);
+        // 将数字转换为长度为6的二进制字符串
+        let binaryIndex = index.toString(2).padStart(4, "0");
+        // 在当前格子中显示二进制数字
+        p.text(
+          binaryIndex,
+          col * cellSize_4 + cellSize_4 / 2,
+          row * cellSize_4 + cellSize_4 / 2
+        );
+      }
+    }
+  };
+}
+
+// index-3
+function sketch4_5(p) {
+  p.setup = function () {
+    // 找到想把该画布插入的位置
+    let divElement = document.getElementById("chessboard4-index-3");
+    let canvas = p.createCanvas(200, 200);
+    canvas.parent(divElement);
+
+    // 计算每个格子的大小
+    cellSize_4 = p.width / boardSize_4;
+    // 设置文本对齐方式为居中
+    p.textAlign(p.CENTER, p.CENTER);
+    // 设置文本大小
+    p.textSize(10);
+    p.frameRate(5);
+  };
+  p.draw = function () {
+    // 设置背景颜色为白色
+    p.background(255);
+    // 遍历棋盘的每一行
+    for (let row = 0; row < boardSize_4; row++) {
+      // 遍历棋盘的每一列
+      for (let col = 0; col < boardSize_4; col++) {
+        // 计算当前格子的索引
+        let index = row * boardSize_4 + col;
+        // 根据索引的奇偶性来设置格子的填充颜色
+        p.fill((index + row) % 2 === 0 ? 200 : 255);
+        if (isBitSet(index, 3)) {
+          p.fill(colors[3]);
+        }
+        // 绘制当前格子
+        p.rect(col * cellSize_4, row * cellSize_4, cellSize_4, cellSize_4);
+        // 设置文本颜色为黑色
+        p.fill(0);
+        // 将数字转换为长度为6的二进制字符串
+        let binaryIndex = index.toString(2).padStart(4, "0");
+        // 在当前格子中显示二进制数字
+        p.text(
+          binaryIndex,
+          col * cellSize_4 + cellSize_4 / 2,
+          row * cellSize_4 + cellSize_4 / 2
+        );
+      }
+    }
+  };
+}
+
+new p5(sketch4_1);
+new p5(sketch4_2);
+new p5(sketch4_3);
+new p5(sketch4_4);
+new p5(sketch4_5);
